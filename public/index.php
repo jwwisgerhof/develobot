851f78367247b7a13a574cff842c3aee2814121b
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-10 15:23:47 +1000

    $_POST fixes

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php';
 $token = "ov1s50h4wQ1HBn4CHhqn83dM";
 
 $response = [
-    "text" => $_SERVER['QUERY_STRING']
+    "text" => file_get_contents("php://input")
 ];
 
 echo json_encode($response);
